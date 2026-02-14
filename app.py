5183b3e2cb85345094cb55d4511aed70019d32b4
Ali Aljanabi <114556000+alialjanabi12@users.noreply.github.com>
2026-02-13 16:24:35 -0800

    Getting the User Data

--- app.py
+++ app.py
@@ -1,17 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
-# -----------------
-# created an app instance
-# -----------------
+"""-----------------
+ created an app instance
+-----------------"""
 app = Flask(__name__)
 
+"""-----------------
+create a route , this url will handle both get and post request
+    - 'post' is when the user submits the form
+    - 'get' is when the user navigates to the page
+    - add <form method="post"> in the html under the form
 
-@app.route("/")
+-----------------"""
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
+    print(request.method)
+    if request.method == "POST":
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        email = request.form["email"]
+        date = request.form["date"]
+        occupation = request.form["occupation"]
+
     return render_template("index.html")
 
 
-# -----------------
-# execute the app
-# -----------------
+"""-----------------
+execute the app
+-----------------"""
 app.run(debug=True, port=5001)
